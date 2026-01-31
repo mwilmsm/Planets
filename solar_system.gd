e5df158e0c82874049a89b87466cb49ab77b4209
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2026-01-31 13:04:21 -0600

    Added constants and made mouse-camera work! -mw

--- solar_system.gd
+++ solar_system.gd
@@ -6,6 +6,9 @@ extends Node3D
 
 var velocity = Vector3.ZERO
 
+var rot_x = 0
+var rot_y = 0
+
 #func _ready():
 	#$SunInstance.scale.x = 139.2
 	#$SunInstance.scale.y = 139.2
@@ -21,20 +24,28 @@ func _process(delta: float) -> void:
 	if (Input.is_action_pressed("escape")):
 		get_tree().quit()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rot_x -= event.screen_relative.x * 0.1
+		rot_y -= event.screen_relative.y * 0.1
+		$Camera3D.transform.basis = Basis()
+		$Camera3D.rotate_object_local(Vector3.UP, rot_x)
+		$Camera3D.rotate_object_local(Vector3.RIGHT, rot_y)
+
 func handleCamera(delta: float) -> void:
 	var acceleration = Vector3.ZERO
 	if (Input.is_action_pressed("camera_back")):
-		acceleration += Vector3.BACK
+		acceleration += $Camera3D.transform.basis.z
 	if (Input.is_action_pressed("camera_forward")):
-		acceleration += Vector3.FORWARD
+		acceleration += -$Camera3D.transform.basis.z
 	if (Input.is_action_pressed("camera_up")):
-		acceleration += Vector3.UP
+		acceleration += $Camera3D.transform.basis.y
 	if (Input.is_action_pressed("camera_down")):
-		acceleration += Vector3.DOWN
+		acceleration += -$Camera3D.transform.basis.y
 	if (Input.is_action_pressed("camera_left")):
-		acceleration += Vector3.LEFT
+		acceleration += -$Camera3D.transform.basis.x
 	if (Input.is_action_pressed("camera_right")):
-		acceleration += Vector3.RIGHT
+		acceleration += $Camera3D.transform.basis.x
 
 	acceleration = acceleration.normalized()
 	velocity *= velocityDegrade
@@ -47,7 +58,7 @@ func handleCamera(delta: float) -> void:
 	if abs(velocity.z) < minVelocity:
 		velocity.z = 0
 
-	$CameraPivot/Camera3D.position += velocity
+	$Camera3D.position += velocity
 
 	if get_node_or_null("Control/SunLabel") != null and get_node_or_null("CameraPivot/Camera3D") != null:
 		$Control/SunLabel.text = str($CameraPivot/Camera3D.position)
